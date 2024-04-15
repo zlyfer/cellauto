56ea28e67538a262ae3a103bd15c0e5b10a6dc55
Frederik Shull <zlyfer@pm.me>
2024-04-15 18:51:26 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'cellauto',
+  webDir: 'www'
+};
+
+export default config;
